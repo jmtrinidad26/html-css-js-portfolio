d27053b60162f11d898320b22758ee46e28ebdd4
jmtrinidad26 <110610062+jmtrinidad26@users.noreply.github.com>
2024-06-02 19:57:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeBtn = document.getElementById('darkModeBtn');
+    
+    darkModeBtn.addEventListener('click', function() {
+        darkModeBtn.classList.toggle('clicked-image');
+        document.body.classList.toggle('dark-mode');
+
+    });
+
+   
+});
